1e274b804dd0e2585df0bc74958318cf670a14af
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-01-22 19:52:56 -0300

    Add files via upload

---
+++ Userspace/login.c
@@ -0,0 +1,73 @@
+#include "../Include/stdint.h"
+#include "../Graphics/graphics.h"
+#include "../Drivers/keyboard.h"
+#include "../Font/text.h" 
+#include "../Memory/mem.h"
+
+#include "GUI/gui.h"
+#include "login.h"
+
+extern char user[];
+
+char userPass[16];
+int loginLoop = 1;
+
+void LoginScreen()
+{
+    DrawRect(0, 0, WSCREEN, HSCREEN, 0x13);
+    
+    int w = 300;
+    int h = 150;
+
+    int x = WSCREEN / 2 - w / 2;
+    int y = HSCREEN / 2 - h / 2 - 50;
+
+    WINDOW login = { "", 0x0F, x, y, w, h };
+
+    DrawRect(x, y, w, h, 0x13);
+
+    y += h / 4;
+
+    //Draw User Icon
+    for (int ys = 0; ys < 32; ys++)
+    {
+        for (int xs = 0; xs < 32; xs++)
+        {
+            BYTE color = user[ys * 32 + (xs + 8)];
+
+            if (color != 0x00 && color != 28)
+            {
+                SetPixel(x + xs * 2, y + ys * 2, color);
+                SetPixel(x + xs * 2 + 1, y + ys * 2, color);
+                SetPixel(x + xs * 2, y + ys * 2 + 1, color);
+                SetPixel(x + xs * 2 + 1, y + ys * 2 + 1, color);
+            }
+        }
+    }
+
+    SetCursorX(x + 70);
+    SetCursorY(y + 20);
+
+    Print("Hello, User!", 0x0F);
+
+    SetCursorX(x);
+    SetCursorY(y + 80);
+
+    Print("Create a password", 0x0F);
+    DrawRoundedRect(x, y + 100, 300, 20, 5, 0x12);
+
+    SetCursorX(x + 5);
+    SetCursorY(y + 100);
+
+    while (loginLoop)
+    {
+
+    }
+}
+
+void SetPassword(char* pass)
+{
+    memcpy(userPass, pass, sizeof(userPass));
+
+    loginLoop = 0x00;
+}
