c0f093a4a520fc72c1c9342497b97851209cd02c
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-01-22 11:46:18 -0300

    Update mem.h

--- Memory/mem.h
+++ Memory/mem.h
@@ -6,3 +6,4 @@ int strncmp(const char *str1, const char *str2, DWORD n);
 DWORD strlen(const char *str);
 char* strcat(char* dest, const char* src);
 char* strcpy(char* dest, const char* src);
+void* memmove(void* dest, const void* src, DWORD n);
