4eba5856baf623636b05d66f5f5f11f0ecd515a1
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-02-04 10:32:43 -0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -33,6 +33,7 @@ i686-elf-gcc -ffreestanding -m32 -g -c Shell/shell.c         -o Binaries/shell.o
 i686-elf-gcc -ffreestanding -m32 -g -c ELF/elf.c             -o Binaries/elf.o
 
 i686-elf-gcc -ffreestanding -m32 -g -c Userspace/userspace.c -o Binaries/userspace.o
+i686-elf-gcc -ffreestanding -m32 -g -c Userspace/desktop.c   -o Binaries/desktop.o
 i686-elf-gcc -ffreestanding -m32 -g -c Userspace/GUI/gui.c   -o Binaries/gui.o
 i686-elf-gcc -ffreestanding -m32 -g -c Userspace/login.c     -o Binaries/login.o
 
@@ -47,7 +48,7 @@ echo "Making Kernel Entry"
 nasm -felf Kernel/entry.asm -o Binaries/entry.o
 
 echo "Linking ELF Files"
-i686-elf-ld -o Binaries/fullkernel.bin -Ttext 0x7E00 Binaries/entry.o Binaries/kernel.o Binaries/panic.o Binaries/asmgdt.o Binaries/gdt.o Binaries/graphics.o Binaries/ports.o Binaries/math.o Binaries/text.o Binaries/mem.o Binaries/alloc.o Binaries/vmm.o Binaries/asmidt.o Binaries/idt.o Binaries/timer.o Binaries/keyboard.o Binaries/cpu.o Binaries/pci.o Binaries/disk.o Binaries/cmos.o Binaries/mouse.o Binaries/qemu.o Binaries/net.o Binaries/memfs.o Binaries/elf.o Binaries/shell.o Binaries/gui.o Binaries/userspace.o Binaries/login.o --oformat binary
+i686-elf-ld -o Binaries/fullkernel.bin -Ttext 0x7E00 Binaries/entry.o Binaries/kernel.o Binaries/panic.o Binaries/asmgdt.o Binaries/gdt.o Binaries/graphics.o Binaries/ports.o Binaries/math.o Binaries/text.o Binaries/mem.o Binaries/alloc.o Binaries/vmm.o Binaries/asmidt.o Binaries/idt.o Binaries/timer.o Binaries/keyboard.o Binaries/cpu.o Binaries/pci.o Binaries/disk.o Binaries/cmos.o Binaries/mouse.o Binaries/qemu.o Binaries/net.o Binaries/memfs.o Binaries/elf.o Binaries/shell.o Binaries/gui.o Binaries/userspace.o Binaries/login.o Binaries/desktop.o --oformat binary
 
 echo "Mounting IMG"
 cat Binaries/boot.bin Binaries/fullkernel.bin > "NovaOS.img"
