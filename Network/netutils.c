ed12f2aaf32af02b9aaf2e4b3ab1d73955a2f573
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-02-09 15:48:01 -0300

    Update netutils.c

--- Network/netutils.c
+++ Network/netutils.c
@@ -22,3 +22,55 @@ DWORD ntohl(DWORD netlong)
            ((netlong & 0x00FF0000) >> 8)  |
            ((netlong & 0xFF000000) >> 24);
 }
+
+WORD ntohs(WORD netshort)
+{
+    return (netshort >> 8) | (netshort << 8);
+}
+
+WORD CalculateChecksum(LPWORD addr, int length)
+{
+    DWORD sum = 0x00;
+
+    while (length > 1)
+    {
+        sum += *addr++;
+        length -= 2;
+    }
+
+    if (length == 1)
+    {
+        sum += *(LPBYTE) addr;
+    }
+
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+
+    return (WORD)(~sum);
+}
+
+DWORD IPStringToDWORD(const char* ip)
+{
+    DWORD result = 0;
+    int part = 0;
+
+    while (*ip) 
+    {
+        if (*ip >= '0' && *ip <= '9') 
+        {
+            part = part * 10 + (*ip - '0');
+        } 
+        else if (*ip == '.') 
+        {
+            result = (result << 8) | (part & 0xFF);
+            
+            part = 0;
+        }
+
+        ip++;
+    }
+
+    result = (result << 8) | (part & 0xFF);
+
+    return result;
+}
