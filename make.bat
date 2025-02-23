3e6282bf014350696aa90669098287b3bceea672
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-02-23 15:04:00 -0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -23,6 +23,7 @@ i686-elf-gcc -ffreestanding -m32 -g -c Timer/timer.c         -o Binaries/timer.o
 i686-elf-gcc -ffreestanding -m32 -g -c Drivers/keyboard.c    -o Binaries/keyboard.o
 i686-elf-gcc -ffreestanding -m32 -g -c Drivers/mouse.c       -o Binaries/mouse.o
 i686-elf-gcc -ffreestanding -m32 -g -c Drivers/qemu.c        -o Binaries/qemu.o
+i686-elf-gcc -ffreestanding -m32 -g -c Drivers/sb16.c        -o Binaries/sb16.o
 i686-elf-gcc -ffreestanding -m32 -g -c Network/netutils.c    -o Binaries/netutils.o
 i686-elf-gcc -ffreestanding -m32 -g -c Network/iptcp.c       -o Binaries/iptcp.o
 i686-elf-gcc -ffreestanding -m32 -g -c Network/icmp.c        -o Binaries/icmp.o
@@ -42,20 +43,22 @@ i686-elf-gcc -ffreestanding -m32 -g -c Userspace/GUI/gui.c   -o Binaries/gui.o
 i686-elf-gcc -ffreestanding -m32 -g -c Userspace/login.c     -o Binaries/login.o
 
 i686-elf-gcc -ffreestanding -m32 -g -c Programs/starfield.c  -o Programs/Binaries/starfield.o
+i686-elf-gcc -ffreestanding -m32 -g -c Programs/bytebeat.c   -o Programs/Binaries/bytebeat.o
 i686-elf-gcc -ffreestanding -m32 -g -c Programs/scroll.c     -o Programs/Binaries/scroll.o
 
 echo "Linking Programs"
 i686-elf-ld -Ttext 0x100000 Programs/Binaries/starfield.o Binaries/graphics.o Binaries/math.o -o Programs/Binaries/stars.elf
 i686-elf-ld -Ttext 0x100000 Programs/Binaries/scroll.o Binaries/graphics.o Binaries/text.o Binaries/math.o -o Programs/Binaries/scroll.elf
+i686-elf-ld -Ttext 0x100000 Programs/Binaries/bytebeat.o Binaries/ports.o Binaries/sb16.o -o Programs/Binaries/bytebeat.elf
 
 echo "Making Kernel Entry"
 nasm -felf Kernel/entry.asm -o Binaries/entry.o
 
 echo "Linking ELF Files"
-i686-elf-ld -o Binaries/fullkernel.bin -Ttext 0x7E00 Binaries/entry.o Binaries/kernel.o Binaries/panic.o Binaries/asmgdt.o Binaries/gdt.o Binaries/graphics.o Binaries/ports.o Binaries/math.o Binaries/text.o Binaries/mem.o Binaries/alloc.o Binaries/vmm.o Binaries/asmidt.o Binaries/idt.o Binaries/timer.o Binaries/keyboard.o Binaries/cpu.o Binaries/pci.o Binaries/disk.o Binaries/cmos.o Binaries/mouse.o Binaries/qemu.o Binaries/netutils.o Binaries/iptcp.o Binaries/net.o Binaries/arp.o Binaries/icmp.o Binaries/memfs.o Binaries/elf.o Binaries/shell.o Binaries/gui.o Binaries/userspace.o Binaries/login.o Binaries/desktop.o --oformat binary
+i686-elf-ld -o Binaries/fullkernel.bin -Ttext 0x7E00 Binaries/entry.o Binaries/kernel.o Binaries/panic.o Binaries/asmgdt.o Binaries/gdt.o Binaries/graphics.o Binaries/ports.o Binaries/math.o Binaries/text.o Binaries/mem.o Binaries/alloc.o Binaries/vmm.o Binaries/asmidt.o Binaries/idt.o Binaries/timer.o Binaries/keyboard.o Binaries/cpu.o Binaries/pci.o Binaries/disk.o Binaries/cmos.o Binaries/mouse.o Binaries/qemu.o Binaries/sb16.o Binaries/netutils.o Binaries/iptcp.o Binaries/net.o Binaries/arp.o Binaries/icmp.o Binaries/memfs.o Binaries/elf.o Binaries/shell.o Binaries/gui.o Binaries/userspace.o Binaries/login.o Binaries/desktop.o --oformat binary
 
 echo "Mounting IMG"
 cat Binaries/boot.bin Binaries/fullkernel.bin > "NovaOS.img"
 
 echo "Running NovaOS (WITH RTL8139 NETWORK DRIVER)"
-qemu-system-i386 -net nic,model=rtl8139 -net user -drive format=raw,file="NovaOS.img" 
+qemu-system-i386 -device sb16 -net nic,model=rtl8139 -net user -drive format=raw,file="NovaOS.img" 
