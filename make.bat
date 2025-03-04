2d55d964f486f35b82021a2f21af1d1c79aa26e0
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-03-04 14:56:04 -0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -62,4 +62,6 @@ echo "Mounting IMG"
 cat Binaries/boot.bin Binaries/fullkernel.bin > "NovaOS.img"
 
 echo "Running NovaOS (WITH RTL8139 NETWORK DRIVER)"
-qemu-system-i386 -device sb16 -net nic,model=rtl8139 -net user -drive format=raw,file="NovaOS.img" 
+
+qemu-img create -f raw disk.raw 256M
+qemu-system-x86_64 -device sb16 -net nic,model=rtl8139 -net user -drive file=NovaOS.img,format=raw,if=ide,index=0 -drive file=disk.raw,format=raw,if=ide,index=2
