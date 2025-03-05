606a040f441d2aa4029906517abc744158e73c87
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-03-05 14:34:45 -0300

    Update graphics.h

--- Graphics/graphics.h
+++ Graphics/graphics.h
@@ -8,5 +8,4 @@ void SetPixel(int x, int y, BYTE color);
 BYTE GetPixel(int x, int y);
 void ClearScreen(void);
 
-void StretchBlt(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
 void DrawRect(int x, int y, int w, int h, BYTE color);
