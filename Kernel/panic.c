6c979a743c542403e3625138f8b8e21caf57e3c7
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-02-17 16:19:58 -0300

    Enhance DeathScreen colors for improved visibility
    Updated the DeathScreen function to change the background color and text color for better visibility.

--- Kernel/panic.c
+++ Kernel/panic.c
@@ -13,15 +13,15 @@
 
 void DeathScreen(const char* error)
 {
-    DrawRect(0, 0, WSCREEN, HSCREEN, 0x28);
+    DrawRect(0, 0, WSCREEN, HSCREEN, 0xFFFF0000);
 
     SetCursorX(0x00);
     SetCursorY(0x00);
 
-    Print("NovaOS has encountered a critical error and needs to shutdown!\n\n", 0x0F);
+    Print("NovaOS has encountered a critical error and needs to shutdown!\n\n", 0xFFFFFFFF);
 
-    Print("Error description: ", 0x0F);
-    Print(error, 0x0F);
+    Print("Error description: ", 0xFFFFFFFF);
+    Print(error, 0xFFFFFFFF);
 
-    Print("\n\nPossible causes:\n - An unexpected event disrupted kernel execution.\n - A critical system component failed to respond.\n - Illegal instructions or unauthorized memory access occurred.\n\nIf the problem persists, please contact the creator of the operating system via Discord: _articzera.\n\nRegisters:\n", 0x0F);
+    Print("\n\nPossible causes:\n - An unexpected event disrupted kernel execution.\n - A critical system component failed to respond.\n - Illegal instructions or unauthorized memory access occurred.\n\nIf the problem persists, please contact the creator of the operating system via Discord: _articzera.\n\nRegisters:\n", 0xFFFFFFFF);
 }
