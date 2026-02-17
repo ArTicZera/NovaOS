f55ea0abb7908b83509e95ad02180690e688bbb3
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-02-17 16:07:17 -0300

    Refactor graphics.h to update color types and add functions

--- Graphics/graphics.h
+++ Graphics/graphics.h
@@ -1,11 +1,11 @@
-//VESA Video Memory
-#define VIDMEM 0xFD000000
-
 #define WSCREEN 800
 #define HSCREEN 600
 
-void SetPixel(int x, int y, BYTE color);
-BYTE GetPixel(int x, int y);
+void InitGraphics(LPDWORD mbinfo);
+DWORD GetFramebuffer();
+DWORD GetPitch();
+void SetPixel(int x, int y, DWORD color);
+DWORD GetPixel(int x, int y);
 void ClearScreen(void);
 
-void DrawRect(int x, int y, int w, int h, BYTE color);
+void DrawRect(int x, int y, int w, int h, DWORD color);
