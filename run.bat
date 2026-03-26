e2aa5d172ec663e35ccb36461801adc0efe838a2
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-03-26 18:25:29 -0300

    Update QEMU commands in run.bat for networking

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 qemu-img create -f raw disk.raw 256M
-qemu-system-x86_64 -device sb16 -net nic,model=rtl8139 -net user -drive file=NovaOS.iso,format=raw,if=ide,index=0 -drive file=disk.raw,format=raw,if=ide,index=2
+qemu-system-x86_64 -device sb16 -netdev user,id=net0 -net nic,model=rtl8139,netdev=net0 -object filter-dump,id=f1,netdev=net0,file=net.pcap -serial stdio -drive file=NovaOS.iso,format=raw,if=ide,index=0 -drive file=disk.raw,format=raw,if=ide,index=2
