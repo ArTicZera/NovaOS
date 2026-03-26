d94a54878014b1bccbfb98435f2beb870e30fa29
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-03-26 18:25:48 -0300

    Update QEMU command for network configuration

--- make.sh
+++ make.sh
@@ -102,4 +102,4 @@ grub-mkrescue -o NovaOS.iso NovaOS/
 #Enable below if you're using linux
 echo "Running NovaOS"
 #qemu-img create -f raw disk.raw 256M
-#qemu-system-x86_64 -device sb16 -net nic,model=rtl8139 -net user -drive file=NovaOS.iso,format=raw,if=ide,index=0 -drive file=disk.raw,format=raw,if=ide,index=2 #-d int
+#qemu-system-x86_64 -device sb16 -netdev user,id=net0 -net nic,model=rtl8139,netdev=net0 -object filter-dump,id=f1,netdev=net0,file=net.pcap -serial stdio -drive file=NovaOS.iso,format=raw,if=ide,index=0 -drive file=disk.raw,format=raw,if=ide,index=2
