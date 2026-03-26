5969f38b69fc592555aba5dc9ff1c2f324ea8ed9
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-03-26 18:05:46 -0300

    Implement backbuffer and blur effect for taskbar
    Added memory allocation for backbuffer and implemented blur effect on taskbar.

--- Userspace/userspace.c
+++ Userspace/userspace.c
@@ -5,11 +5,13 @@
 #include "../Drivers/keyboard.h"
 #include "../Drivers/mouse.h"
 #include "../Memory/mem.h"
+#include "../Memory/alloc.h"
 #include "../Font/text.h"
 #include "../FileSystem/memfs.h"
 #include "../Timer/timer.h"
 #include "../Drivers/sb16.h"
 
+#include "GUI/win.h"
 #include "GUI/gui.h"
 #include "login.h"
 #include "desktop.h"
@@ -29,10 +31,12 @@ int isStartMenuOpen = 0;
 int isMaximizedOpen = 0;   
 int wasMousePressed = 0;
 
-int currentWindowID = 0;
+int currentPID = 0;
 int totalWindows = 0;
 
 DWORD startBuffer[150 * 200];
+
+DWORD* backbuffer;
 
 void DrawBackground()
 {
@@ -60,23 +64,15 @@ void DrawBackground()
 
 void DrawTaskbar()
 {
+    DWORD* blurbuffer = AllocateMemory(WSCREEN * HSCREEN * 4);
+
+    // 1️⃣ aplica blur NO BACKBUFFER
+    BlurRegion(backbuffer, blurbuffer, 0, HSCREEN - 35, WSCREEN, 35, 1);
+
     for (int y = HSCREEN - 35; y < HSCREEN; y++)
     {
-        for (int x = 0; x < WSCREEN; x++)
-        {
-            SetPixel(x, y, 0xFF2F2F2F);
-        }
+        memcpy(&backbuffer[y * WSCREEN], &blurbuffer[y * WSCREEN], WSCREEN * 4);
     }
-
-    SetCursorX(30);
-    SetCursorY(HSCREEN - 25);
-
-    Print("START", 0xFFFFFFFF);
-
-    SetCursorX(WSCREEN - 65);
-    SetCursorY(HSCREEN - 25);
-
-    GetCMOSDate();
 }
 
 void SaveStartArea()
@@ -127,118 +123,27 @@ void StartButtonHandler(int x, int y, int pressed)
     wasMousePressed = pressed;
 }
 
-void GetWindow(WINDOW window)
-{
-    winmgr[currentWindowID] = window;
-
-    currentWindowID++;
-    totalWindows++;
-}
-
-void MaximizeWindow(int x, int y, int pressed)
-{
-    const int buttonSize = 10;
-
-    for (int i = 0; i < totalWindows; i++)
-    {
-        WINDOW* win = &winmgr[i];
-
-        int buttonXStart = win->x + win->w - (2 * buttonSize) - 20;
-        int buttonXEnd = buttonXStart + buttonSize;
-        int buttonYStart = win->y + 5;
-        int buttonYEnd = buttonYStart + buttonSize;
-
-        if (pressed && x >= buttonXStart && x < buttonXEnd && y >= buttonYStart && y < buttonYEnd)
-        {
-            if (win->isMaximized % 2 == 0)
-            {
-                //SaveTotalArea();
-
-                win->x = 0;
-                win->y = 0;
-                win->w = WSCREEN;
-                win->h = HSCREEN - 32;
-
-                win->isMaximized++;
-
-                DrawWindow(*win, 0x00);
-            }
-            else
-            {
-                win->x = GetStartWindowXY();
-                win->y = GetStartWindowXY();
-                win->w = 320;
-                win->h = 200;
-
-                win->isMaximized++;
-
-                DrawBackground();
-
-                DrawWindow(*win, 0x00);
-            }
-
-            if (strcmp(win->title, "NovaOS - Run") == 0x00)
-            {
-                //StartShell(*win);
-            }
-        }
-    }
-}
-
-void CloseWindow(int x, int y, int pressed)
-{
-    const int buttonSize = 10;
-
-    for (int i = 0; i < totalWindows; i++)
-    {
-        WINDOW* win = &winmgr[i];
-
-        int buttonXStart = win->x + win->w - buttonSize - 10;
-        int buttonXEnd = buttonXStart + buttonSize;
-        int buttonYStart = win->y + 5;
-        int buttonYEnd = buttonYStart + buttonSize;
-
-        if (pressed && x >= buttonXStart && x < buttonXEnd && y >= buttonYStart && y < buttonYEnd)
-        {
-            for (int j = i; j < totalWindows - 1; j++)
-            {
-                winmgr[j] = winmgr[j + 1];
-            }
-
-            totalWindows--;
-
-            UpdateExplorer();
-
-            break;
-        }
-    }
-}
-
-void MinimizeWindow(int x, int y, int pressed)
-{
-    const int buttonSize = 10;
-
-    for (int i = 0; i < totalWindows; i++)
-    {
-        WINDOW* win = &winmgr[i];
-
-        int buttonXStart = win->x + win->w - (3 * buttonSize) - 30;
-        int buttonXEnd = buttonXStart + buttonSize;
-        int buttonYStart = win->y + 5;
-        int buttonYEnd = buttonYStart + buttonSize;
-
-        if (pressed && x >= buttonXStart && x < buttonXEnd && y >= buttonYStart && y < buttonYEnd)
-        {
-            UpdateExplorer();
-        }
-    }
-}
-
 void UpdateExplorer()
 {
     DrawBackground();
-    DrawTaskbar();
     DesktopIcons();
+    DrawTaskbar();
+
+    for (int y = HSCREEN - 35; y < HSCREEN; y++)
+    {
+        for (int x = 0; x < WSCREEN; x++)
+        {
+            SetPixel(x, y, backbuffer[y * WSCREEN + x]);
+        }
+    }
+
+    SetCursorX(30);
+    SetCursorY(HSCREEN - 25);
+    Print("START", 0xFFFFFFFF);
+
+    SetCursorX(WSCREEN - 65);
+    SetCursorY(HSCREEN - 25);
+    GetCMOSDate();
 }
 
 void UserspaceState(int state)
@@ -309,8 +214,8 @@ void DesktopIcons()
                 icon = ICON_DEFAULT;
             }
 
-            int x = 10;
-            int y = 10 + (index * 76);
+            int x = 64;
+            int y = 20 + (index * 76);
             DrawDesktopIcon(icon, filename, x, y);
             SetDesktopIndex(filename, 0x01, x, y);
 
@@ -319,45 +224,7 @@ void DesktopIcons()
     }
 }
 
-void HandleWindowDragging(WINDOW* window, int mouseX, int mouseY, int mousePressed)
-{
-    static int isDragging = 0;
-    static int offsetX = 0;
-    static int offsetY = 0;
-    
-    if (!window) return;
 
-    if (mousePressed) 
-    {
-        if (!isDragging) 
-        {
-            if (mouseX >= window->x && mouseX <= window->x + window->w - 100 && mouseY >= window->y && mouseY <= window->y + 20) 
-            {
-                isDragging = 1;
-                offsetX = mouseX - window->x;
-                offsetY = mouseY - window->y;
-            }
-        }
-
-        if (isDragging) 
-        {
-            window->x = mouseX - offsetX;
-            window->y = mouseY - offsetY;
-
-            if (window->x < 0) window->x = 0;
-            if (window->y < 0) window->y = 0;
-            if (window->x + window->w > WSCREEN) window->x = WSCREEN - window->w;
-            if (window->y + window->h > HSCREEN) window->y = HSCREEN - window->h;
-
-            UpdateExplorer();
-            DrawWindow(*window, 0);
-        }
-    } 
-    else 
-    {
-        isDragging = 0;
-    }
-}
 
 void DrawBootScr(void)
 {
@@ -400,32 +267,21 @@ void UserSpace()
 
     LoginScreen();
 
-    /*BYTE audio[44100];
-
-    for (int i = 0; i < sizeof(audio); i++)
-    {
-        audio[i] = bootAudio[i];
-    }
-
-    PlayBytebeat(audio, sizeof(audio));*/
-
     KeyboardState(1);
 
     UpdateExplorer();
 
-    while (true)
+    int lastPressed = 0;
+
+    while (TRUE)
     {
         int x, y, pressed;
         GetMouseState(&x, &y, &pressed);
 
-        DesktopEvents(x, y, pressed);
-
         StartButtonHandler(x, y, pressed);
 
-        MaximizeWindow(x, y, pressed);
-        MinimizeWindow(x, y, pressed);
-        CloseWindow(x, y, pressed);
+        HandleMouseClick(x, y, pressed);
 
-        HandleWindowDragging(&winmgr[currentWindowID - 1], x, y, pressed);
+        HandleWindowDragging(x, y, pressed);
     }
 }
