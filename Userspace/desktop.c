cffc03398812b5090482401444909d568892f27f
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-03-26 18:03:48 -0300

    Refactor desktop event handling and update includes

--- Userspace/desktop.c
+++ Userspace/desktop.c
@@ -9,6 +9,7 @@
 #include "../Memory/mem.h"
 #include "../FileSystem/memfs.h"
 
+#include "GUI/win.h"
 #include "GUI/gui.h"
 #include "userspace.h"
 #include "desktop.h"
@@ -41,73 +42,5 @@ void SetDesktopIndex(char* name, int type, int x, int y)
         items[totItems].y = y;
 
         totItems++;
-    }
-}
-
-void DesktopEvents(int x, int y, int pressed)
-{
-    if (pressed)
-    {
-        DesktopItem* item = GetClickedItem(x, y);
-        
-        if (item)
-        {
-            if (item->type == 0)
-            {
-                char title[32];
-                const char* baseTitle = "Explorer - ";
-                int i = 0;
-
-                while (*baseTitle != '\0') 
-                {
-                    title[i++] = *baseTitle++;
-                }
-
-                const char* dirName = item->name;
-
-                while (*dirName != '\0') 
-                {
-                    title[i++] = *dirName++;
-                }
-
-                title[i] = '\0';
-
-                int pos = GetStartWindowXY();
-                WINDOW window = { title, 0xFF101010, pos, pos, 350, 200 };
-                DrawWindow(window, 0x00);
-                GetWindow(window);
-
-                FileSystem* fs = (FileSystem*) FSADDRESS;
-                Directory* selectedDir = NULL;
-
-                for (int i = 0; i < MAXSUBDIR; i++)
-                {
-                    if (fs->root.subdirs[i] != NULL && strcmp(fs->root.subdirs[i]->name, item->name) == 0)
-                    {
-                        selectedDir = fs->root.subdirs[i];
-                        break;
-                    }
-                }
-
-                if (selectedDir != NULL)
-                {
-                    int fileX = pos + 10;
-                    int fileY = pos + 40;
-
-                    for (int i = 0; i < MAXFILES; i++)
-                    {
-                        if (selectedDir->files[i].filename[0] != '\0')
-                        {
-                            DrawDesktopIcon(ICON_DEFAULT, selectedDir->files[i].filename, fileX, fileY);
-                            fileX += 100;
-                        }
-                    }
-                }
-            }
-            else if (item->type == 1)
-            {
-                Print(item->name, 0x0B);
-            }
-        }
     }
 }
