1955475d7f5e5cc3b786efda3850b692802a85ef
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-06-24 22:12:39 -0300

    Add files via upload

---
+++ Userspace/GUI/win.h
@@ -0,0 +1,47 @@
+#ifndef WIN_H
+#define WIN_H
+
+#define MAX_WINDOWS 10
+
+typedef struct WINDOW
+{
+    const char* title;
+    DWORD color;
+
+    int x, y;
+    int w, h;
+
+    int dragOffsetX;
+    int dragOffsetY;
+
+    int visible;
+    int dragging;
+    int focused;
+
+    LPDWORD buffer;
+
+    struct WINDOW* next;
+    struct WINDOW* prev;
+} WINDOW;
+
+extern int StartWindowXY;
+
+void DrawWindow(WINDOW window, int nextWindow);
+void AddWindow(WINDOW* window);
+void BringToFront(WINDOW* win);
+void HandleMouseClick(int x, int y, int pressed);
+void DrawAllWindows();
+WINDOW* GetWindowAt(int x, int y);
+WINDOW* CreateWindow(int x, int y,int w, int h, DWORD color, char* title);
+void DrawRoundedRect(int x, int y, int w, int h, int radius, DWORD color);
+void HandleWindowDragging(int mouseX, int mouseY, int mousePressed);
+
+void DrawButton(int x, int y, int size, const char* icon, DWORD color);
+void DrawWindowButtons(WINDOW window);
+void CloseWindow(int x, int y, int pressed);
+
+void SaveBackgroundArea(WINDOW* win);
+void SaveWindowArea(WINDOW* win);
+void RestoreWindowArea(WINDOW* win);
+
+#endif
