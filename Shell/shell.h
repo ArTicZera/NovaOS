b10c18c44cdcb5273969ccb1e09207b327608a01
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-06-24 22:07:28 -0300

    Refactor shell.h to include GUI support

--- Shell/shell.h
+++ Shell/shell.h
@@ -1,10 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include "../Userspace/GUI/win.h"
+
+extern int shellNOGUI;
+extern int winshellX;
+extern int winshellY;
+
 void StartShellNoGUI();
+void StartShellGUI(WINDOW* win);
 void PrintWelcomeMSG();
 void PrintCurrentDir();
-void ProcessShellCMD(char* command);
+void ProcessShellCMD(char* command, int x, int y);
 void ProcessShellRun(char* proccess);
 
 #endif
