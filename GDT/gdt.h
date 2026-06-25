a75cb75759ea8afbc91abe7af96eddcac1aef0f6
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-06-24 21:41:21 -0300

    Refactor GDT header with new type definitions

--- GDT/gdt.h
+++ GDT/gdt.h
@@ -1,47 +1,52 @@
+#ifndef GDT_H
+#define GDT_H
+
 struct gdt_entry_struct{
-    uint16_t limit;
-    uint16_t base_low;
-    uint8_t base_middle;
-    uint8_t access;
-    uint8_t flags;
-    uint8_t base_high;
+    WORD limit;
+    WORD base_low;
+    BYTE base_middle;
+    BYTE access;
+    BYTE flags;
+    BYTE base_high;
 }__attribute__((packed));
 
 struct gdt_ptr_struct{
-    uint16_t limit;
-    unsigned int base;
+    WORD limit;
+    DWORD base;
 }__attribute__((packed));
 
 struct tss_entry_struct{
-	uint32_t prev_tss;
-	uint32_t esp0;
-	uint32_t ss0;
-	uint32_t esp1;
-	uint32_t ss1;
-	uint32_t esp2;
-	uint32_t ss2;
-	uint32_t cr3;
-	uint32_t eip;
-	uint32_t eflags;
-	uint32_t eax;
-	uint32_t ecx;
-	uint32_t edx;
-	uint32_t ebx;
-	uint32_t esp;
-	uint32_t ebp;
-	uint32_t esi;
-	uint32_t edi;
-	uint32_t es;
-	uint32_t cs;
-	uint32_t ss;
-	uint32_t ds;
-	uint32_t fs;
-	uint32_t gs;
-	uint32_t ldt;
-	uint32_t trap;
-	uint32_t iomap_base;
+	DWORD prev_tss;
+	DWORD esp0;
+	DWORD ss0;
+	DWORD esp1;
+	DWORD ss1;
+	DWORD esp2;
+	DWORD ss2;
+	DWORD cr3;
+	DWORD eip;
+	DWORD eflags;
+	DWORD eax;
+	DWORD ecx;
+	DWORD edx;
+	DWORD ebx;
+	DWORD esp;
+	DWORD ebp;
+	DWORD esi;
+	DWORD edi;
+	DWORD es;
+	DWORD cs;
+	DWORD ss;
+	DWORD ds;
+	DWORD fs;
+	DWORD gs;
+	DWORD ldt;
+	DWORD trap;
+	DWORD iomap_base;
 } __attribute__((packed));
 
 void InitGDT();
-void setGdtGate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
-void writeTSS(uint32_t num, uint16_t ss0, uint32_t esp0);
+void setGdtGate(DWORD num, DWORD base, DWORD limit, BYTE access, BYTE gran);
+void writeTSS(DWORD num, WORD ss0, DWORD esp0);
+
+#endif
