302eca5f013a621a549a970a4647a9bdb5e30a10
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-06-28 15:24:36 -0300

    Delete Network directory

--- Network/NOTE.md
+++
@@ -1,1 +0,0 @@
-The network driver is OK but its failing to ping some IPs. If anyone could fix that I would appreciate it!
--- Network/netutils.h
+++
@@ -1,8 +0,0 @@
-WORD htons(WORD hostshort);
-WORD ntohs(WORD netshort);
-
-DWORD htonl(DWORD hostlong);
-DWORD ntohl(DWORD netlong);
-
-WORD CalculateChecksum(LPWORD addr, int length);
-DWORD IPStringToDWORD(const char* ip);
