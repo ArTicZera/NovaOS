1b783fcd5c99c0a1694e9845fe0eed9b6e2b758a
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-07-05 19:17:59 -0300

    Update doomgeneric for NovaOS with input handling
    Refactor doomgeneric for NovaOS with key handling and input queue.

--- DOOM/doomgeneric_novaos.c
+++ DOOM/doomgeneric_novaos.c
@@ -1,10 +1,15 @@
-// doomgeneric for Allegro library
+// doomgeneric for NovaOS
 
 #include "doomkeys.h"
 #include "i_system.h"
 #include "i_video.h"
 #include "m_argv.h"
 #include "doomgeneric.h"
+#include "doom_input.h"
+
+#include "../Include/stdint.h"
+#include "../Include/ports.h"
+#include "../Interrupts/idt.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -12,8 +17,12 @@
 #include <unistd.h>
 #include <alloc.h>
 #include <string.h>
+#include <ctype.h>
 
-static bool videomode_set = false;
+static unsigned char keyDown[128] = {0};
+static unsigned short s_KeyQueue[128];
+static unsigned int s_KeyQueueWriteIndex = 0;
+static unsigned int s_KeyQueueReadIndex = 0;
 static unsigned int* temp_bitmap = NULL;
 
 void DG_Init() 
@@ -33,6 +42,53 @@ void DG_DrawFrame()
             temp_bitmap[scrY * 1280 + scrX] = DG_ScreenBuffer[y * DOOMGENERIC_RESX + x];
         }
     }
+}
+
+void addKeyToQueue(int pressed, unsigned int keyCode)
+{
+	unsigned char key = convertToDoomKey(keyCode);
+
+	unsigned short keyData = (pressed << 8) | key;
+
+	s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
+	s_KeyQueueWriteIndex++;
+	s_KeyQueueWriteIndex %= 128;
+}
+
+unsigned char convertToDoomKey(unsigned int key)
+{
+	switch(key)
+	{
+		case 0x11: // W
+		case 0x48:
+			return KEY_UPARROW;
+
+		case 0x1F:
+		case 0x50: // S
+			return KEY_DOWNARROW;
+
+		case 0x1E:
+		case 0x4B: // A
+			return KEY_LEFTARROW;
+
+		case 0x20: // D
+		case 0x4D:
+			return KEY_RIGHTARROW;
+
+		case 0x39: // Space
+			return KEY_USE;
+
+		case 0x1C: // Enter
+			return KEY_ENTER;
+
+		case 0x1D: //co
+			return KEY_FIRE;
+
+		case 0x01: // Esc
+			return KEY_ESCAPE;
+	}
+
+	return key;
 }
 
 
@@ -55,17 +111,29 @@ uint32_t DG_GetTicksMs()
 }
 
 
-int DG_GetKey(int *pressed, unsigned char *doomKey)
-{
-	return 0;
-}
-
-
 void DG_SetWindowTitle(const char *title)
 {
 	
 }
 
+int DG_GetKey(int* pressed, unsigned char* doomKey)
+{
+	if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex)
+	{
+		return 0;
+	}
+	else
+	{
+		unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
+		s_KeyQueueReadIndex++;
+		s_KeyQueueReadIndex %= 128;
+
+		*pressed = keyData >> 8;
+		*doomKey = keyData & 0xFF;
+
+		return 1;
+	}
+}
 
 int _start(int argc, char** argv)
 {
@@ -77,8 +145,15 @@ int _start(int argc, char** argv)
 	while (true)
 	{
 		doomgeneric_Tick();
+		//Read scan code
+		unsigned char scan = inb(0x60);
 
-		extern unsigned int DG_GetTickMs();
+		//Check if its pressed
+		unsigned char isPress = !(scan & 0x80);
+		scan &= 0x7F;
+
+		addKeyToQueue(isPress, scan);
+		//I_StartFrame ();
 	}
 
 	return 0;
