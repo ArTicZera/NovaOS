3c40cb4eab8ac3d3b64185ffe3709fcb887bfb0c
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-09-06 14:39:24 -0300

    Add text buffer drawing functions to text.h
    Added new functions for drawing text buffers and printing with specific parameters.

--- Font/text.h
+++ Font/text.h
@@ -4,7 +4,10 @@
 #define BYTES_PER_ROW ((WFONT + 7) / 8)
 #define GLYPH_SIZE (BYTES_PER_ROW * HFONT)
 
-// extension by nicolasbickhoff11
+void DrawCharBuffer(DWORD* buffer, int w, int h, int x, int y, char c, DWORD color);
+void DrawTextBuffer(DWORD *buffer, int w, int h, int x, int y, const char *text, DWORD color);
+void WLPrint(DWORD* buffer, int x, int y, int w, int h, const char* str, DWORD color);
+void PrintBuffer(DWORD* buffer, int w, int h, int x, int y, const char* str, DWORD color);
 
 void DrawChar(BYTE* bitmap, DWORD color);
 void Print(const char* str, DWORD color);
