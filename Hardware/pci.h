bd103b4b6df1d2544af1008a326c0814c9bbe56e
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-09-06 14:43:06 -0300

    Refactor pci.h to include header guards and new functions

--- Hardware/pci.h
+++ Hardware/pci.h
@@ -1,5 +1,19 @@
+#ifndef PCI_H
+#define PCI_H
+
 #define PCI_CONFIG_ADDRESS 0xCF8
 #define PCI_CONFIG_DATA 0xCFC
 
+typedef struct
+{
+    DWORD base;
+    BYTE  isIO;
+} PCIBar;
+
+
 DWORD PCIConfigReadWord(BYTE bus, BYTE slot, BYTE func, BYTE offset);
+BYTE PCIConfigReadByte(BYTE bus, BYTE slot, BYTE func, BYTE offset);
+PCIBar ReadBarPCI(BYTE bus, BYTE slot, BYTE func, BYTE barIndex);
 void ShowPCIDevices();
+
+#endif
