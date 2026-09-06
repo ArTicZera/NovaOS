822ff477a29a9375314a9af712167bfc8f57ab2e
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-09-06 15:00:15 -0300

    Update shell.c
    spoilers!! (not ready)

--- Shell/shell.c
+++ Shell/shell.c
@@ -34,11 +34,6 @@
 #include "../ELF/bin.h"
 #include "../ELF/run.h"
 
-#include "../novasm/lexer.h"
-#include "../novasm/parser.h"
-#include "../novasm/encoder.h"
-#include "../novasm/assembler.h"
-
 #include "../Wayland/af_unix.h"
 #include "../Wayland/server.h"
 #include "../Wayland/client.h"
