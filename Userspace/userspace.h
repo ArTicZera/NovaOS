54bf7658427d2213cdc80caed6182dda5a1c9f8a
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-09-06 15:02:42 -0300

    Add files via upload

---
+++ Userspace/userspace.h
@@ -0,0 +1,17 @@
+#ifndef USERSPACE_H
+#define USERSPACE_H
+
+void StartButtonHandler(int x, int y, int pressed);
+
+void UserSpace();
+void DrawBackground();
+void DrawTaskbar();
+void UpdateExplorer();
+void UserspaceState(int state);
+void DesktopIcons();
+void DrawBootScr();
+
+void SaveStartArea();
+void RestoreStartArea();
+
+#endif
