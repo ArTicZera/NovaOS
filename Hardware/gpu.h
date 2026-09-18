b587218cb13cc03a11e6ee9befb11b0342bd5513
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-18 10:10:34 -0300

    Change function parameters to use pointers for GPU info

--- Hardware/gpu.h
+++ Hardware/gpu.h
@@ -2,11 +2,13 @@
 #define AMD 0x1002
 #define NVIDIA 0x10DE
 
-void GetGPUVendor(char vendor);
-void GetGPUModel(char model);
-void GetGPUName(char name);
+void GetGPUVendor(char *vendor);
+void GetGPUModel(char *model);
+void GetGPUName(char *name);
 //void GetGPUVideoRAM();
 // Pls, make VRAM detect
 
 void ShowGPUInfo();
 void ShowGPUName();
+
+void DetectGPU();
