324c61b39ee8cd4992bc6823d917859e64bf0d8a
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-18 09:58:45 -0300

    Define GPU vendors and declare related functions
    Added GPU vendor and model definitions and function declarations.

--- Hardware/gpu.h
+++ Hardware/gpu.h
@@ -1,1 +1,12 @@
-void efsd
+#define INTEL 0x8086
+#define AMD 0x1002
+#define NVIDIA 0x10DE
+
+void GetGPUVendor(char vendor);
+void GetGPUModel(char model);
+void GetGPUName(char name);
+//void GetGPUVideoRAM();
+// Pls, make VRAM detect
+
+void ShowGPUInfo();
+void ShowGPUName();
