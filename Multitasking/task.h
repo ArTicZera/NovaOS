ce970f57cc27bea14606044179a57399e417555e
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-09-22 16:36:02 -0300

    Define task management structures and functions

--- Multitasking/task.h
+++ Multitasking/task.h
@@ -1,1 +1,32 @@
+#ifndef TASK_H
+#define TASK_H
 
+#define MAX_TASKS    16
+#define TASK_UNUSED  0
+#define TASK_READY   1
+#define TASK_RUNNING 2
+#define TASK_DEAD    3
+
+typedef struct
+{
+    DWORD esp;
+    DWORD ebp;
+    DWORD eip;
+    DWORD cr3;
+    DWORD pid;
+    DWORD st;
+} Task;
+
+void SchedulerInit(void);
+
+int CreateTask(void(*entry)(void), DWORD stackTop);
+
+DWORD Scheduler(DWORD currentESP);
+
+Task* GetCurrentTask(void);
+
+void ExitTask(void);
+
+int TaskCount(void);
+
+#endif
